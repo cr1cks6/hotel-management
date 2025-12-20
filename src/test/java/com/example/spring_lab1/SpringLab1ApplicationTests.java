6f6cd34466ee559da9f4507da02e0f4847a28264
Алексей <coolpro126@gmail.com>
2025-12-20 14:09:12 +0300

    Initial commit: Spring Lab 1 - Hello World and Time Controller

---
+++ src/test/java/com/example/spring_lab1/SpringLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
